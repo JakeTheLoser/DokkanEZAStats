754e69cebde01d618847ef085e49254f6c5b7ec0
Owner <Owner@DESKTOP-FN0IG81>
2022-09-01 18:42:26 -0400

    Add project files.

---
+++ dokkanbattle/DokkanEZA.cpp
@@ -0,0 +1,48 @@
+// Dokkan Battle Bot for EZA Stats
+// 9/1/2022
+// Jacob Cogan
+
+#include <iostream>
+using namespace std; 
+
+int main() {
+	
+		int firstNumber, secondNumber, sub;
+		cout << "Enter Max Level Stat NO HIDDEN POTENTIAL! Then enter level 1 ATK stat. ";
+
+		cin >> firstNumber >> secondNumber;
+		sub = firstNumber - secondNumber;
+
+		double dokkan = 0.4839;
+		sub * dokkan; 
+		double product = sub * dokkan; 
+		product + firstNumber; 
+		cout << product + firstNumber; 
+
+		
+
+		
+		
+
+
+
+
+		
+
+
+
+			
+
+
+	
+
+		
+		
+
+
+
+
+
+
+	
+}
